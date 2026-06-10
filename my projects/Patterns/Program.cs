78c9b7134b59a7ca7763403864112ebe7180ec6a
Mustafa Abdelgawwad <mustafa.abdelgawwwad@gmail.com>
2026-06-11 00:03:49 +0300

    Add files via upload

---
+++ my projects/Patterns/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+// ======= Pyramid ======= \\
+
+for (int i = 10; i >= 1; i--)
+{
+    for (int j = 1; j <= 10 - i; j++)
+    {
+        Console.Write(" ");
+    }
+    for (int k = 1; k <= i; k++)
+    {
+        Console.Write("* ");
+    }
+    Console.WriteLine();
+}
+
+// ======= Reversed Pyramid ======= \\
+
+  for (int i = 10; i >= 1; i--)
+{
+    for(int j = 1; j <= 10 - i; j++)
+    {
+        Console.Write(" ");
+    }
+    for(int k = 1; k <= i; k++)
+    {
+        Console.Write("* ");
+    }
+    Console.WriteLine();
+}
+
+// ======= Hollow Pyramid ======= \\
+
+  int rows = 10;
+
+for (int i = 1; i <= rows; i++)
+{
+    for (int j = 1; j <= rows - i; j++)
+    {
+        Console.Write(" ");
+    }
+
+    for (int k = 1; k <= i; k++)
+    {
+        if (k == 1 || k == i || i == rows)
+        {
+            Console.Write("* ");
+        }
+        else
+        {
+            Console.Write("  ");
+        }
+    }
+    Console.WriteLine();
+}*/
