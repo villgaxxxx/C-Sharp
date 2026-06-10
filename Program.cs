be0f5d0a5db87c7e3171854ea0f5b655081529ee
Mustafa Abdelgawwad <mustafa.abdelgawwwad@gmail.com>
2026-06-10 21:52:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿
+
+// ======= FOR LOOP ======= \\
+
+/*Console.WriteLine("Hello, World!");
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+}
+
+int num = 4, sum = 0;
+for (int i = 0; i < num; i++)
+{
+    sum+= i;
+}
+Console.WriteLine($"Sum = {sum}");
+
+
+int i = 0;
+for(;i<5;)
+{
+    Console.WriteLine("For loop I = " + i);
+    i++;
+}
+
+int num = 4, sum = 0;
+for (int i = 10; i > num; i--)
+{
+    sum += i;
+}
+Console.WriteLine($"Sum = {sum}");
+
+
+// ======= WHILE LOOP ======== \\
+
+int i = 0;
+while(i < 5)
+{
+    Console.WriteLine(i);
+    i++;
+}
+
+
+// ======= DO WHILE LOOP ======== \\
+
+int i = 1 , n = 2 , result = 0;
+do
+{
+    result = n * i;
+    Console.WriteLine($"{n} * {i} = {result}");
+    i++;
+}
+while(i <= 20)
+
+
+// ======= NESTED LOOPS ======== \\
+
+for (int i = 1; i <= 8; i++)
+{
+    for (int j = 1; j <= 8; j++)
+    {
+        Console.Write("* ");
+    }
+    Console.WriteLine();
+}*/
