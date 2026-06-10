4b6a4c669274a8ec089657a279a23ec9efd7b3b0
Mustafa Abdelgawwad <mustafa.abdelgawwwad@gmail.com>
2026-06-10 21:57:51 +0300

    Add files via upload

---
+++ my projects/Multiple Tables/Program.cs
@@ -0,0 +1,9 @@
+﻿for(int i = 1; i <= 12; i++)
+{
+    Console.WriteLine($"======= Table {i} =======");
+    for(int j = 1; j <= 12; j++)
+    {
+        Console.WriteLine($"{i} * {j} = {i*j}");
+    }
+    Console.WriteLine();
+}
